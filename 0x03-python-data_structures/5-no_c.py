e3070051bff938d9a00b3912e4d14be91708c98d
AbduGad <abdo79gad@gmail.com>
2023-09-09 18:44:26 +0300

    removed comment in 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    #return (my_string.replace("cC", " "))
     new_string = ""
     for i in range(len(my_string)):
         if my_string[i] not in "cC":
