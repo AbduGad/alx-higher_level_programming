52c374defa6231e8271352e24e375a7b84be3701
AbduGad <abdo79gad@gmail.com>
2023-09-10 23:49:16 +0300

    pystyle

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     mul = []
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
-            mul.append(True)
+            mul += True
         else:
-            mul.append(False)
+            mul += False
     return mul
