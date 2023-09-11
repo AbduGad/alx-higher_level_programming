7903b3f77e8bba30627171dbdc7d1d5283129070
AbduGad <abdo79gad@gmail.com>
2023-09-11 22:35:47 +0300

    comiple error in 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -43,3 +43,22 @@ int is_palindrome(listint_t **head)
 	}
 	return (1);
 }
+
+/**
+ * list_len - Returns the number of elements in a linked `list_t` list
+ * @head: head of linked list
+ * Return: number of elements in list
+ */
+size_t list_len(const listint_t *head)
+{
+	size_t v;
+
+	v = 0;
+	while (head != NULL)
+	{
+		head = head->next;
+		v++;
+	}
+
+	return (v);
+}
