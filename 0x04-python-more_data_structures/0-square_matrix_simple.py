c0af4af9a480903ef06f49b7a3b4613d8822b3de
AbduGad <abdo79gad@gmail.com>
2023-09-13 19:52:42 +0300

    04 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+	new = matrix[:]
+	for i in range(len(matrix)):
+		temp = []
+		for j in range(len(matrix[i])):
+			temp.append(matrix[i][j] ** 2)
+		new.append(temp)
+	return new
