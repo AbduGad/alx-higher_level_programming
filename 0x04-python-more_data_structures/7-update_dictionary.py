00a612467a3fdb2862ecd4b9a752120f97880304
AbduGad <abdo79gad@gmail.com>
2023-09-13 22:14:32 +0300

    04 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""if key in a_dictionary (will work) 
+&& update updates it if it exists or 
+creates the new one"""
 def update_dictionary(a_dictionary, key, value):
     for i in a_dictionary:
         if i == key:
             a_dictionary[key] = value
-            return
+            return a_dictionary
     a_dictionary.update({key : value})
+    return a_dictionary
