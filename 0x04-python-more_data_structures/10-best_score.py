7d82505ba5308d27c1c4f7a83e65d48514fdfda3
AbduGad <abdo79gad@gmail.com>
2023-09-13 23:14:02 +0300

    04 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    max_score = 0
+    max_key = None
+    for i, j in a_dictionary.items():
+        if j > max_score:
+            max_key = i
+            max_score = j
+    return max_key
