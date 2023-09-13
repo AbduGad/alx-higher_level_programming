1b2c621355fca933388c2cf15a1acae45acb0e15
AbduGad <abdo79gad@gmail.com>
2023-09-14 00:21:46 +0300

    04 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                  'C': 100, 'D': 500, 'M': 1000}
+    prev = 0
+    sum = 0
+    if type(roman_string) is not str or roman_string is None:
+        return 0
+    for i in roman_string:
+        if roman_dict[i] > prev:
+            sum += roman_dict[i] - prev * 2
+        else:
+            sum += roman_dict[i]
+        prev = roman_dict[i]
+    return sum
