0da38d8ab02700bab0599f38289e7e1aefecf551
AbduGad <abdo79gad@gmail.com>
2023-10-02 18:01:51 +0300

    test

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,8 +5,8 @@
 class Rectangle:
     """rectangle class with priv height and width"""
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     def width(self):
         """geeter for width"""
