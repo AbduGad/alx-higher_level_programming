46e25a3b1f34e208c5e783e70c1a7efcfbe687a8
AbduGad <abdo79gad@gmail.com>
2023-10-10 19:57:45 +0300

    tasks

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+""" write to file"""
+
+
+def write_file(filename="", text=""):
+    """writes to a utf-8 encoded text file
+    """
+    with open(filename, 'w', encoding='utf-8') as myFile:
+        return myFile.write(text)
