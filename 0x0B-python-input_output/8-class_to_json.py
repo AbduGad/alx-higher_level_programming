42733bdf34d8632f8e28991ddf7861acdd6a412a
AbduGad <abdo79gad@gmail.com>
2023-10-10 20:10:41 +0300

    pycode style

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python3
-""" load json to object module
-    Functions:
-        load_from_json: open and return a read json
-"""
-
-import json
+"""class to JSON"""
 
 
-def load_from_json_file(filename):
-    """ Returns a read json
-        Args:
-            filename: json file
-    """
-
-    with open(filename, encoding='utf-8') as file:
-        return json.loads(file.read())
+def class_to_json(obj):
+    """classes to json"""
+    return (obj.__dict__)
