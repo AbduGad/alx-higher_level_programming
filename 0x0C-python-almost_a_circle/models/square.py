e7ff1004307a16061bf0a106b338ea94457f4fa3
AbduGad <abdo79gad@gmail.com>
2023-10-16 20:49:23 +0300

    square doc

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,15 +12,19 @@ class Square(Rectangle):
 
     @property
     def size(self):
+        """size"""
         return self.width
 
     @size.setter
     def size(self, size):
+        """size"""
         self.width = size
         self.height = size
 
     def __str__(self):
+        """str"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
 
     def to_dictionary(self):
+        """dictionary"""
         return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
