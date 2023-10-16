225817aeb207639949e4883cd2998112beef99ad
AbduGad <abdo79gad@gmail.com>
2023-10-16 20:41:45 +0300

    pycodestyle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,75 +3,73 @@
 import json
 
 
-
 class Base():
-	"""Base Class for inheritance."""
-	
-	__nb_objects = 0
+    """Base Class for inheritance."""
 
-	def __init__(self, id=None):
-		""" instance constructor
-        """
-		if id is not None:
-			self.id = id
-		else:
-			Base.__nb_objects += 1
-			self.id = self.__nb_objects
-	
-	@staticmethod
-	def to_json_string(list_dictionaries):
-		""" Write a Json file with list_objs
-            Args:
-                cls: class name
-                list_objs: list of instances
-        """
-		if list_dictionaries in (None, []):
-			return "[]"
-		else:
-			return json.dumps(list_dictionaries)
+    __nb_objects = 0
 
-	@classmethod
-	def save_to_file(cls, list_objs):
-		""" Save the content into a JSON file"""
-		save = []
-		if list_objs:
-			for obj in list_objs:
-				save.append(obj.to_dictionary())
-		with open(cls.__name__ + ".json", 'w') as file:
-			file.write(Base.to_json_string(save))
-	
-	@staticmethod
-	def from_json_string(json_string):
-		""" Read a Json string and return a list of objects"""
-		if json_string is None:
-			return []
-		return json.loads(json_string)
-	
-	@classmethod
-	def create(cls, **dictionary):
-		""" Create and set a new instance
-            Args:
-                **dictionary: kwargs of class
-        """
-		if cls.__name__ == "Rectangle":
-			dummy = cls(0)
-		elif cls.__name__ == "Square":
-			dummy = cls(0)
-		dummy.update(**dictionary)
-		return dummy
+    def __init__(self, id=None):
+        """ instance constructor"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = self.__nb_objects
 
-	@classmethod
-	def load_from_file(cls):
-		""" Instantiate and return a list of
-            instances from json file
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ Write a Json file with list_objs
+        Args:
+        cls: class name
+        list_objs: list of instances
         """
-		inst_list = []
-		try:
-			with open(cls.__name__ + ".json", 'r') as file:
-				data = file.read()
-				list = Base.from_json_string(data)
-				for kwargs in list:
-					inst_list.append(cls.create(**kwargs))
-				return inst_list
-		except FileNotFoundError:
-			return []
+        if list_dictionaries in (None, []):
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ Save the content into a JSON file"""
+        save = []
+        if list_objs:
+            for obj in list_objs:
+                save.append(obj.to_dictionary())
+        with open(cls.__name__ + ".json", 'w') as file:
+            file.write(Base.to_json_string(save))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ Read a Json string and return a list of objects"""
+        if json_string is None:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ Create and set a new instance
+        Args:
+        **dictionary: kwargs of class
+        """
+        if cls.__name__ == "Rectangle":
+            dummy = cls(0)
+        elif cls.__name__ == "Square":
+            dummy = cls(0)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """ Instantiate and return a list of
+    instances from json file
+"""
+        inst_list = []
+        try:
+            with open(cls.__name__ + ".json", 'r') as file:
+                data = file.read()
+                list = Base.from_json_string(data)
+                for kwargs in list:
+                    inst_list.append(cls.create(**kwargs))
+                return inst_list
+        except FileNotFoundError:
+            return []
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
 """square module"""
-from rectangle import Rectangle
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-	"""Square class"""
+    """Square class"""
 
-	def __init__(self, size, x=0, y=0, id=None):
-		"""initializes"""
-		super().__init__(size, size, x, y, id)
+    def __init__(self, size, x=0, y=0, id=None):
+        """initializes"""
+        super().__init__(size, size, x, y, id)
 
-	@property
-	def size(self):
-		return self.width
+    @property
+    def size(self):
+        return self.width
 
-	@size.setter
-	def size(self, size):
-		self.width = size
-		self.height = size
+    @size.setter
+    def size(self, size):
+        self.width = size
+        self.height = size
 
-	def __str__(self):
-		return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
-	
-	def to_dictionary(self):
-		return {"id" : self.id, "size" : self.size, "x": self.x, "y" : self.y}
+    def __str__(self):
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    def to_dictionary(self):
+        return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
