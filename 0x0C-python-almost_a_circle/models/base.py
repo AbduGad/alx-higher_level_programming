ee939486dfc2937b149d63261fb7874480c2f59f
AbduGad <abdo79gad@gmail.com>
2023-10-16 18:57:13 +0300

    modules created

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,62 @@
 #!/usr/bin/python3
+"""base class"""
+import json
+
+
+
+class Base():
+	"""Base Class for inheritance."""
+	__nb_objects = 0
+
+	def __init__(self, id=None):
+		""" instance constructor
+        """
+		if id is not None:
+			self.id = id
+		else:
+			Base.__nb_objects += 1
+			self.id = self.__nb_objects
+	
+	@staticmethod
+	def to_json_string(list_dictionaries):
+		if list_dictionaries in (None, []):
+			return "[]"
+		else:
+			return json.dumps(list_dictionaries)
+
+	@classmethod
+	def save_to_file(cls, list_objs):
+		save = []
+		if list_objs:
+			for obj in list_objs:
+				save.append(obj.to_dictionary())
+		with open(cls.__name__ + ".json", 'w') as file:
+			file.write(Base.to_json_string(save))
+	
+	@staticmethod
+	def from_json_string(json_string):
+		if json_string is None:
+			return []
+		return json.loads(json_string)
+	
+	@classmethod
+	def create(cls, **dictionary):
+		if cls.__name__ == "Rectangle":
+			dummy = cls(0)
+		elif cls.__name__ == "Square":
+			dummy = cls(0)
+		dummy.update(**dictionary)
+		return dummy
+
+	@classmethod
+	def load_from_file(cls):
+		inst_list = []
+		try:
+			with open(cls.__name__ + ".json", 'r') as file:
+				data = file.read()
+				list = Base.from_json_string(data)
+				for kwargs in list:
+					inst_list.append(cls.create(**kwargs))
+				return inst_list
+		except FileNotFoundError:
+			return []
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,1 +1,109 @@
 #!/usr/bin/python3
+"""Rectangle module"""
+
+from base import Base
+
+
+
+class Rectangle(Base):
+	"""Rectangle class inherits from Base"""
+
+	def __init__(self, width, height, x=0, y=0, id=None):
+		""" Rectangle Class constructor
+            Args:
+                width: first size
+                height: second size
+                x: first position
+                y: second position
+                id: identification
+        """
+		super().__init__(id)
+		self.width = width
+		self.height = height
+		self.x = x
+		self.y = y
+	
+	@property
+	def width(self):
+		"""git width"""
+		return self.__width
+	
+	@property
+	def height(self):
+		"""get height"""
+		return self.__height
+	
+	@property
+	def x(self):
+		"""get x"""
+		return self.__x
+	
+	@property
+	def y(self):
+		"""get y"""
+		return self.__y
+	
+	@width.setter
+	def width(self,  width):
+		"""set width"""
+		if not isinstance(width, int):
+			raise TypeError("width must be an integer")
+		if width <= 0:
+			raise ValueError("width must be > 0")
+		self.__width = width
+	
+	@height.setter
+	def height(self, height):
+		"""set height"""
+		if not isinstance(height, int):
+			raise TypeError("height must be an integer")
+		if height <= 0:
+			raise ValueError("height must be > 0")
+		self.__height = height
+
+	@x.setter
+	def x(self, x):
+		"""set x"""
+		if not isinstance(x, int):
+			raise TypeError("x must be an integer")
+		if x < 0:
+			raise ValueError("x must be >= 0")
+		self.__x = x
+	
+	@y.setter
+	def y(self, y):
+		"""set y"""
+		if not isinstance(y, int):
+			raise TypeError("y must be an integer")
+		if y < 0:
+			raise ValueError("y must be >= 0")
+		self.__y = y
+	
+	def area(self):
+		"""calculates rectangle area"""
+		return self.width * self.height
+	
+	def display(self):
+		""" prints Rectangle instance with character # """
+		print(('#' * self.width + '\n') * self.height, end = "")
+
+	def __str__(self):
+		return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+
+	def display(self):
+		print(("" + '\n') * self.y, end = "")
+		print(((self.x * " ") + ('#' * self.width + '\n')) * self.height, end = "")
+
+	def update(self, *args, **kwargs):
+		if args:
+			atr_list = ["id", "width", "height",
+			 "x", "y"]
+			for i, each in enumerate(args):
+				setattr(self, atr_list[i], each)
+		else:
+			for key, value in kwargs.items():
+				setattr(self, key, value)
+
+	def to_dictionary(self):
+		return {"id": self.id,"width": self.width,"height": self.height,
+		  "x": self.x,"y": self.y}
