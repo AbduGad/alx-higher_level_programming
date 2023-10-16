1048e88fc3b3bff2a220c9f3c64049dc829daad4
AbduGad <abdo79gad@gmail.com>
2023-10-16 20:44:29 +0300

    t

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -88,7 +88,7 @@ class Rectangle(Base):
     def __str__(self):
         """__str__"""
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} -\
-            {self.width}/{self.height}"
+             {self.width}/{self.height}"
 
     def display(self):
         """display"""
