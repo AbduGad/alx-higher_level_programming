0c775fe36931ed6ac6e6d1a30fa12dbcf1080c30
AbduGad <abdo79gad@gmail.com>
2023-11-14 19:01:41 +0200

    test

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,3 @@
 -- List by best
 -- Show score ordered and name from second_table
-SELECT score, name FROM second_table
-    ORDER BY score DESC, name;
+SELECT score, name FROM second_table ORDER BY score DESC;
