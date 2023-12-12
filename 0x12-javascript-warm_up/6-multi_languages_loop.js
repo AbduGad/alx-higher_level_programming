f0f1bcaf9fd9b0b4104de0f5528107153f75efc9
AbduGad <abdo79gad@gmail.com>
2023-12-12 02:12:36 +0200

    semistandard

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-/*myVar.forEach((element) => {
+/* myVar.forEach((element) => {
   console.log(element);
-});*/
+}); */
 for (let i = 0; i < myVar.length; i++) {
-	console.log(myVar[i]);
+  console.log(myVar[i]);
 }
