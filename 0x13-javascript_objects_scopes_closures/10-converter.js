30e824819c4a2ba0819d8c888533253a03afead8
AbduGad <abdo79gad@gmail.com>
2023-12-12 22:11:49 +0200

    test

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-exports.converter = function converter (base) {
-  if (typeof converter.Num === 'undefined') {
-    converter.Num = base;
-    return converter;
-  } else {
-    return parseInt(converter.Num, base);
-  }
+exports.converter = function (base) {
+	return function (dec) {
+		return dec.toString(base);
+	};
 };
