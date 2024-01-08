93c3d9d6f616ecc5db02a78d69cd82b73bd54a40
AbduGad <abdo79gad@gmail.com>
2024-01-08 18:33:16 +0200

    fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const args = process.argv.slice(2).map(Number);
+const numArgs = args.length;
+
+if (numArgs === 0) {
+  console.log(0);
+} else if (numArgs === 1) {
+  console.log(0);
+} else {
+  const sortedArgs = args.sort((a, b) => b - a);
+  console.log(sortedArgs[1]);
+}
