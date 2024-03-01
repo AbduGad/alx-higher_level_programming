0a426c01d4dacc3aed91db615231bdafd006b609
AbduGad <abdo79gad@gmail.com>
2024-03-01 23:26:05 +0200

    try

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,15 +6,13 @@ import requests
 from sys import argv
 
 
-def main(argv):
-    """
-    Sends a POST request to the passed URL with the email as a parameter,
-    and displays the body of the response (decoded in utf-8)
-    """
-    values = {'email': argv[2]}
-    url = argv[1]
-    r = requests.post(url, data=values)
-    print(r.text)
-
 if __name__ == "__main__":
-    main(argv)
+	def main(argv):
+		"""
+		Sends a POST request to the passed URL with the email as a parameter,
+		and displays the body of the response (decoded in utf-8)
+		"""
+		values = {'email': argv[2]}
+		url = argv[1]
+		r = requests.post(url, data=values)
+		print(r.text)
