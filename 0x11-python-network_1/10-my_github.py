58048adbc048f16f4d5176ec4b314adb25dfa61b
AbduGad <abdo79gad@gmail.com>
2024-03-01 23:27:47 +0200

    reverted

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,18 +7,20 @@ from requests.auth import HTTPBasicAuth
 from sys import argv
 
 
+def main(argv):
+    """
+    Script that takes your GitHub credentials (username and password) and
+    uses the GitHub API to display your id.
+    """
+    user = argv[1]
+    password = argv[2]
+    response = requests.get('https://api.github.com/user',
+                            auth=HTTPBasicAuth(user, password))
+    try:
+        profile_info = response.json()
+        print(profile_info['id'])
+    except:
+        print('None')
+
 if __name__ == "__main__":
-	def main(argv):
-		"""
-		Script that takes your GitHub credentials (username and password) and
-		uses the GitHub API to display your id.
-		"""
-		user = argv[1]
-		password = argv[2]
-		response = requests.get('https://api.github.com/user',
-								auth=HTTPBasicAuth(user, password))
-		try:
-			profile_info = response.json()
-			print(profile_info['id'])
-		except:
-			print('None')
+    main(argv)
--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,15 +6,19 @@ import requests
 from sys import argv
 
 
+def main(argv):
+    """
+    Method that manage urllib.error.HTTPError exceptions and
+    print: Error code: followed by the HTTP status code
+    """
+    url = argv[1]
+    r = requests.get(url)
+    if r.status_code == requests.codes.ok:
+        print(r.text)
+    else:
+        print("Error code: {}".format(r.status_code))
+
+
 if __name__ == "__main__":
-	def main(argv):
-		"""
-		Method that manage urllib.error.HTTPError exceptions and
-		print: Error code: followed by the HTTP status code
-		"""
-		url = argv[1]
-		r = requests.get(url)
-		if r.status_code == requests.codes.ok:
-			print(r.text)
-		else:
-			print("Error code: {}".format(r.status_code))
+    main(argv)
+	
