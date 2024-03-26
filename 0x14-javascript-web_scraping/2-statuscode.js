b2b8d6e7d7264d11647ab6259af5afec802dc336
AbduGad <abdo79gad@gmail.com>
2024-03-26 17:17:55 +0200

    task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+const request = require( 'request' );
+const process = require( "process" );
+
+request(process.argv[2],  (err, res, body) => {
+	console.log('code:', res.statusCode);
+});
