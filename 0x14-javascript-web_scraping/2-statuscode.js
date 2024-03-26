bf3733a16565005dcbf27287842534b6e0d9ed71
AbduGad <abdo79gad@gmail.com>
2024-03-26 17:29:59 +0200

    test

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
-const process = require('process');
 const request = require('request');
-
-let url = process.argv[2];
-
-request(url, function (error, response, body) {
-  if (error != null) {
-    console.log(error);
-  } else {
-    console.log('code:', response.statusCode);
+const urlElement = process.argv[2];
+request.get(urlElement, function (error, response) {
+  if (error) {
+    return console.log(error);
   }
+  console.log('code: %d', response && response.statusCode);
 });
 /*const request = require('request');
 const process = require('process');
