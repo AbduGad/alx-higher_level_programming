803a83633c4fa854fe1504b4e433f24f4839713c
AbduGad <abdo79gad@gmail.com>
2024-03-26 18:02:50 +0200

    task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+
+request(url + process.argv[2] + '/', (err, res, bdy) => {
+  if (err | !err) { console.log(JSON.parse(bdy).title); }
+});
