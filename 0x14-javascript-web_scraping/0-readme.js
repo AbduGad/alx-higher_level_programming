9530ecbba143f6ea1efc8aeedb179d91a41ce9e6
AbduGad <abdo79gad@gmail.com>
2024-03-26 16:57:59 +0200

    semistandard fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,8 +3,5 @@ const fs = require('fs');
 const process = require('process');
 
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-	if (err)
-		console.log(err);
-	else
-		console.log(data);
+  if (err) { console.log(err); } else { console.log(data); }
 });
