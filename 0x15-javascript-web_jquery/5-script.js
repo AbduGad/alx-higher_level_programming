22544e20aea9f1cd007c2582b7deb239138d4acb
AbduGad <abdo79gad@gmail.com>
2024-04-15 00:43:11 +0200

    postmortm

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -2,6 +2,6 @@
 
 $(document).ready(function() {
     $('DIV#add_item').click(function() {
-        $('<li>').text('Item').appendTo('.my_list');
+        $('<li>').text('Item').appendTo('UL.my_list');
     });
 });
